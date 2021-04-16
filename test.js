0b21969496753364f30e8798bf989e7f95a7859d
rkemery-hashicorp <rick.kemery@hashicorp.com>
2021-04-16 02:43:41 -0400

    new mocha test

--- test.js
+++ test.js
@@ -1,9 +1,38 @@
-var expect  = require('chai').expect;
-var request = require('request');
+var server = require('/usr/src/app');
 
-it('Main page content', function(done) {
-    request('http://localhost:3000' , function(error, response, body) {
-        expect(body).to.equal('Hello World!');
-        done();
+describe('server', function () {
+  before(function () {
+    server.listen(3000);
+  });
+
+  after(function () {
+    server.close();
+  });
+});
+
+var assert = require('assert'),
+    http = require('http');
+
+describe('/', function () {
+  it('should return 200', function (done) {
+    http.get('http://localhost:3000', function (res) {
+      assert.equal(200, res.statusCode);
+      done();
     });
+  });
+
+  it('should say "Hello World!"', function (done) {
+    http.get('http://localhost:3000', function (res) {
+      var data = '';
+
+      res.on('data', function (chunk) {
+        data += chunk;
+      });
+
+      res.on('end', function () {
+        assert.equal('Hello World!\n', data);
+        done();
+      });
+    });
+  });
 });
