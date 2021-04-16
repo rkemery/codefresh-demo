207eb86255d664cdec241208811a0ea65eef432f
rkemery-hashicorp <rick.kemery@hashicorp.com>
2021-04-16 01:34:15 -0400

    fix mocha test

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const request = require("supertest")("http://127.0.0.1:3000");
+const request = require("supertest")("http://localhost:3000");
 const expect = require("chai").expect;
 
 describe("GET /", function () {
