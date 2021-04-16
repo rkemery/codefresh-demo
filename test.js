65005eae4b15f6c1d6c1d2272b9adac780143d08
rkemery-hashicorp <rick.kemery@hashicorp.com>
2021-04-16 02:48:23 -0400

    edit path

--- test.js
+++ test.js
@@ -3,14 +3,14 @@ var assert = require('assert'),
 
 describe('/', function () {
   it('should return 200', function (done) {
-    http.get('http://localhost:3000', function (res) {
+    http.get('/', function (res) {
       assert.equal(200, res.statusCode);
       done();
     });
   });
 
   it('should say "Hello World!"', function (done) {
-    http.get('http://localhost:3000', function (res) {
+    http.get('/', function (res) {
       var data = '';
 
       res.on('data', function (chunk) {
