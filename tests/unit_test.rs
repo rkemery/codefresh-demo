4c8be5d5a31c78aff59b2e5941c065079a381e45
rkemery-hashicorp <rick.kemery@hashicorp.com>
2021-04-17 01:32:08 -0400

    test

--- tests/unit_test.rs
+++ tests/unit_test.rs
@@ -3,15 +3,6 @@ extern crate hyper;
 use hyper::*;
 
 #[test]
-fn post_backend() {
-    let client = Client::new();
-
-    let res = client.post("http://localhost:8000/health").body(r#"{ "msg": "Ok" }"#).send().unwrap();
-    
-    assert_eq!(res.status, hyper::Ok);
-}
-
-#[test]
 fn get_backend() {
     let client = Client::new();
 
