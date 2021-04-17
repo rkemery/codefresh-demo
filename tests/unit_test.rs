36c1fca999342ef558c1facc2db4ab7139a10dd3
rkemery-hashicorp <rick.kemery@hashicorp.com>
2021-04-17 00:51:09 -0400

    fix unit test

--- tests/unit_test.rs
+++ tests/unit_test.rs
@@ -1,23 +1,21 @@
-use httptest::{Server, Expectation, matchers::*, responders::*};
-// Start a server running on a local ephemeral port.
-let server = Server::run();
-// Configure the server to expect a single GET /health request and respond
-// with a 200 status code.
-server.expect(
-    Expectation::matching(request::method_path("GET", "/health"))
-    .respond_with(status_code(200)),
-);
+extern crate hyper;
 
-// The server provides server.addr() that returns the address of the
-// locally running server, or more conveniently provides a server.url() method
-// that gives a fully formed http url to the provided path.
-let url = server.url("/health");
-let client = hyper::Client::new();
-// Issue the GET /foo to the server.
-let resp = client.get(url).await.unwrap();
+use hyper::*;
 
-// assert the response has a 200 status code.
-assert!(resp.status().is_success());
+#[test]
+fn post_backend() {
+    let client = Client::new();
 
-// on Drop the server will assert all expectations have been met and will
-// panic if not.
+    let res = client.post("http://localhost:8000/health").body(r#"{ "msg": "Ok" }"#).send().unwrap();
+    
+    assert_eq!(res.status, hyper::Ok);
+}
+
+#[test]
+fn get_backend() {
+    let client = Client::new();
+
+    let res = client.get("http://localhost:8000/health").send().unwrap();
+
+    assert_eq!(res.status, hyper::Ok);
+}
